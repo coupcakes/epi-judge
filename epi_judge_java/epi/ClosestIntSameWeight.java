2d38bbdda625ebcff80f97983e2f6d6f28d6a2fd
coupcakes <suhcheollie@gmail.com>
2024-02-18 21:58:51 -0800

    Fix 4.4 constant solution

--- epi_judge_java/epi/ClosestIntSameWeight.java
+++ epi_judge_java/epi/ClosestIntSameWeight.java
@@ -6,29 +6,30 @@ import epi.test_framework.GenericTest;
 public class ClosestIntSameWeight {
   @EpiTest(testDataFile = "closest_int_same_weight.tsv")
   public static long closestIntSameBitCount(long x) {
+    return closestIntSameBitCountConstant(x);
     // TODO - you fill in here.
-    int NUM_OF_UNSIGNED_BITS = 63;
-    for (int i = 0; i < NUM_OF_UNSIGNED_BITS - 1; i++) {
-      if (((x >>> i) & 1) != ((x >>> (i + 1)) & 1)) {
-        // swap bits
-        x ^= ((1L << i) | (1L << (i + 1)));
-        return x;
-      }
-    }
-    throw new IllegalArgumentException("Value contains all 0's or all 1's");
+    // int NUM_OF_UNSIGNED_BITS = 63;
+    // for (int i = 0; i < NUM_OF_UNSIGNED_BITS - 1; i++) {
+    // if (((x >>> i) & 1) != ((x >>> (i + 1)) & 1)) {
+    // // swap bits
+    // x ^= ((1L << i) | (1L << (i + 1)));
+    // return x;
+    // }
+    // }
+    // throw new IllegalArgumentException("Value contains all 0's or all 1's");
   }
 
   public static long closestIntSameBitCountConstant(long x) {
     long mask = 0;
     if ((x & 1) == 1) {
       // get the rightmost 0
-      mask = (x + 1) | ((x + 1) >>> 1);
+      mask = (x + 1) & ~x;
     } else {
       // get the rightmost 1
-      mask = (~x + 1) | ((~x + 1) >>> 1);
+      mask = (~x + 1) & x;
     }
 
-    return x ^ mask;
+    return x ^ (mask | (mask >>> 1));
   }
 
   public static void main(String[] args) {
