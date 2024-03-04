8d06f21f9776cc2b6094155edf0dfa1450dd7e84
coupcakes <suhcheollie@gmail.com>
2024-03-04 11:36:31 -0800

    Add 5.4 Arrays advance by offsets solution

--- epi_judge_java/epi/AdvanceByOffsets.java
+++ epi_judge_java/epi/AdvanceByOffsets.java
@@ -1,20 +1,27 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class AdvanceByOffsets {
   @EpiTest(testDataFile = "advance_by_offsets.tsv")
   public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
-    // TODO - you fill in here.
-    return true;
+    int furthestIndexSoFar = 0;
+    int lastIndex = maxAdvanceSteps.size() - 1;
+    for (int i = 0; i <= furthestIndexSoFar && furthestIndexSoFar < lastIndex; i++) {
+      furthestIndexSoFar = Math.max(furthestIndexSoFar, maxAdvanceSteps.get(i) + i);
+    }
+    return furthestIndexSoFar >= lastIndex;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "AdvanceByOffsets.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
