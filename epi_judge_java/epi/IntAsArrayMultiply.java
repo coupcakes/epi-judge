2d214add8a059de2de32fa7a8176359ab7a0f6d9
coupcakes <suhcheollie@gmail.com>
2024-03-03 20:36:37 -0800

    Add 5.3 Arrays int as array multiply

--- epi_judge_java/epi/IntAsArrayMultiply.java
+++ epi_judge_java/epi/IntAsArrayMultiply.java
@@ -1,20 +1,51 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
 public class IntAsArrayMultiply {
   @EpiTest(testDataFile = "int_as_array_multiply.tsv")
   public static List<Integer> multiply(List<Integer> num1, List<Integer> num2) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> res = new ArrayList<Integer>(Collections.nCopies(num1.size() + num2.size(), 0));
+
+    final int signOfRes = num1.get(0) < 0 ^ num2.get(0) < 0 ? -1 : 1;
+    num1.set(0, Math.abs(num1.get(0)));
+    num2.set(0, Math.abs(num2.get(0)));
+
+    for (int i = num1.size() - 1; i >= 0; i--) {
+      for (int j = num2.size() - 1; j >= 0; j--) {
+        int ans = res.get(i + j + 1) + (num1.get(i) * num2.get(j));
+        int nextAns = res.get(i + j) + (ans / 10);
+        res.set(i + j, nextAns);
+        res.set(i + j + 1, ans % 10);
+      }
+    }
+
+    // remove extra 0's
+    int startIndex = 0;
+    while (startIndex < res.size() && res.get(startIndex) == 0) {
+      startIndex++;
+    }
+    res = res.subList(startIndex, res.size());
+    if (res.isEmpty()) {
+      return Arrays.asList(0);
+    }
+
+    res.set(0, res.get(0) * signOfRes);
+    return res;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "IntAsArrayMultiply.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
