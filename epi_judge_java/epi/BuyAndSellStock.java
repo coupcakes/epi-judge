00d9452b7ab97e216c7e2fee3668ffe3df5ae7e5
coupcakes <suhcheollie@gmail.com>
2024-03-05 10:07:21 -0800

    Add 5.6 buy and sell stock solution

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -1,20 +1,30 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    int minIndex = 0;
+    double maxProfit = 0.0;
+    for (int i = 1; i < prices.size(); i++) {
+      if (prices.get(i) < prices.get(minIndex)) {
+        minIndex = i;
+      }
+      maxProfit = Math.max(maxProfit, prices.get(i) - prices.get(minIndex));
+    }
+    return maxProfit;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "BuyAndSellStock.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
