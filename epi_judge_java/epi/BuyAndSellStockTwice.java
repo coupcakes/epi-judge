c30aad5a40b8ba8a38f37c20397ab9e5246871ee
coupcakes <suhcheollie@gmail.com>
2024-03-06 12:55:29 -0800

    Add 5.7 buy and sell stock twice solution

--- epi_judge_java/epi/BuyAndSellStockTwice.java
+++ epi_judge_java/epi/BuyAndSellStockTwice.java
@@ -1,20 +1,32 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class BuyAndSellStockTwice {
   @EpiTest(testDataFile = "buy_and_sell_stock_twice.tsv")
   public static double buyAndSellStockTwice(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    double minPrice1 = -prices.get(0);
+    double maxProfit1 = 0;
+    double minPrice2 = -prices.get(0);
+    double maxProfit2 = 0;
+    for (int i = 1; i < prices.size(); i++) {
+      minPrice1 = Math.max(minPrice1, -prices.get(i));
+      maxProfit1 = Math.max(maxProfit1, prices.get(i) + minPrice1);
+      minPrice2 = Math.max(minPrice2, maxProfit1 - prices.get(i));
+      maxProfit2 = Math.max(maxProfit2, prices.get(i) + minPrice2);
+    }
+    return maxProfit2;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "BuyAndSellStockTwice.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
