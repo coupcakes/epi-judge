d57135242c729f88afb2e57d777032cd3f82e513
coupcakes <suhcheollie@gmail.com>
2024-03-13 17:39:44 -0700

    Add 5.11 next permutation solution

--- epi_judge_java/epi/NextPermutation.java
+++ epi_judge_java/epi/NextPermutation.java
@@ -1,20 +1,42 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
+
 public class NextPermutation {
   @EpiTest(testDataFile = "next_permutation.tsv")
   public static List<Integer> nextPermutation(List<Integer> perm) {
-    // TODO - you fill in here.
-    return null;
+    // find longest decreasing suffix on the right
+    int k = perm.size() - 2;
+    while (k >= 0 && perm.get(k) >= perm.get(k + 1)) {
+      k--;
+    }
+    if (k == -1) {
+      return Collections.emptyList(); // given perm is the last one
+    }
+
+    // swap item at k with next largest item in decreasing suffix
+    for (int i = perm.size() - 1; i > k; i--) {
+      if (perm.get(i) > perm.get(k)) {
+        Collections.swap(perm, i, k);
+        break;
+      }
+    }
+
+    Collections.reverse(perm.subList(k + 1, perm.size()));
+
+    return perm;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "NextPermutation.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
