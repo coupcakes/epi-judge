a89c7642f0c1f2d9a43e296686db89e204ce4459
coupcakes <suhcheollie@gmail.com>
2024-03-28 09:19:25 -0700

    Add 6.5 Is string palindromic punctuation java solution

--- epi_judge_java/epi/IsStringPalindromicPunctuation.java
+++ epi_judge_java/epi/IsStringPalindromicPunctuation.java
@@ -1,11 +1,31 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsStringPalindromicPunctuation {
   @EpiTest(testDataFile = "is_string_palindromic_punctuation.tsv")
 
   public static boolean isPalindrome(String s) {
     // TODO - you fill in here.
+    int start = 0, end = s.length() - 1;
+    while (start < end) {
+      while (!Character.isLetterOrDigit(s.charAt(start)) && start < end) {
+        start++;
+      }
+
+      while (!Character.isLetterOrDigit(s.charAt(end)) && start < end) {
+        end--;
+      }
+
+      if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+        return false;
+      }
+
+      start++;
+      end--;
+    }
+
     return true;
   }
 
@@ -13,7 +33,8 @@ public class IsStringPalindromicPunctuation {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "IsStringPalindromicPunctuation.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
