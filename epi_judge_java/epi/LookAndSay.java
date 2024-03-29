782db646f057a5dd73d905c88b5b151229279b7c
coupcakes <suhcheollie@gmail.com>
2024-03-28 18:59:26 -0700

    Add 6.7 look and say java solution

--- epi_judge_java/epi/LookAndSay.java
+++ epi_judge_java/epi/LookAndSay.java
@@ -1,19 +1,38 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class LookAndSay {
   @EpiTest(testDataFile = "look_and_say.tsv")
 
   public static String lookAndSay(int n) {
-    // TODO - you fill in here.
-    return "";
+    String s = "1";
+    for (int i = 1; i < n; i++) {
+      s = nextNum(s);
+    }
+    return s;
+  }
+
+  private static String nextNum(String s) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < s.length(); i++) {
+      int count = 1;
+      while (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1)) {
+        i++;
+        count++;
+      }
+      sb.append(count).append(s.charAt(i));
+    }
+    return sb.toString();
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "LookAndSay.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
