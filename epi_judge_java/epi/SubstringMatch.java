b00873d2141e7157ab40dccc299b83ebc062ca26
coupcakes <suhcheollie@gmail.com>
2024-04-02 18:29:39 -0700

    Add 6.12 substring match java solution

--- epi_judge_java/epi/SubstringMatch.java
+++ epi_judge_java/epi/SubstringMatch.java
@@ -1,21 +1,50 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class SubstringMatch {
   @EpiTest(testDataFile = "substring_match.tsv")
 
   // Returns the index of the first character of the substring if found, -1
   // otherwise.
   public static int rabinKarp(String t, String s) {
-    // TODO - you fill in here.
-    return 0;
+    if (s.length() > t.length()) {
+      return -1;
+    }
+    final int BASE = 26;
+    int sPow = 1;
+    int sHash = 0;
+    int tHash = 0;
+    // calculate hash for substrings
+    for (int i = 0; i < s.length(); i++) {
+      sPow = i > 0 ? sPow * BASE : 1;
+      sHash = sHash * BASE + s.charAt(i);
+      tHash = tHash * BASE + t.charAt(i);
+    }
+
+    // go thru and compute rolling hash
+    for (int i = s.length(); i < t.length(); i++) {
+      if (tHash == sHash && t.substring(i - s.length(), i).equals(s)) {
+        return i - s.length();
+      }
+      tHash -= sPow * t.charAt(i - s.length());
+      tHash = tHash * BASE + t.charAt(i);
+    }
+
+    if (tHash == sHash && t.substring(t.length() - s.length()).equals(s)) {
+      return t.length() - s.length();
+    }
+
+    return -1;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "SubstringMatch.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
