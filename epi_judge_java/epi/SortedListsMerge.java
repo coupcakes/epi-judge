683c82cfb79585c305ef8ddcaf5fe258f84abbc2
coupcakes <suhcheollie@gmail.com>
2024-04-03 17:58:41 -0700

    Add 7.1 Sorted list merge java solution

--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -1,20 +1,36 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class SortedListsMerge {
   @EpiTest(testDataFile = "sorted_lists_merge.tsv")
-  //@include
+  // @include
   public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
-                                                      ListNode<Integer> L2) {
-    // TODO - you fill in here.
-    return null;
+      ListNode<Integer> L2) {
+    ListNode<Integer> dummy = new ListNode<Integer>(0, null);
+    ListNode<Integer> curr = dummy;
+    while (L1 != null && L2 != null) {
+      if (L1.data <= L2.data) {
+        curr.next = L1;
+        L1 = L1.next;
+      } else {
+        curr.next = L2;
+        L2 = L2.next;
+      }
+      curr = curr.next;
+    }
+    curr.next = L1 != null ? L1 : L2;
+
+    return dummy.next;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "SortedListsMerge.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
