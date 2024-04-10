0510104aedd87e98bd7efd40e7d7e6a18cfb9e67
coupcakes <suhcheollie@gmail.com>
2024-04-10 11:29:46 -0700

    Add 7.11 is list palindromic java solution

--- epi_judge_java/epi/IsListPalindromic.java
+++ epi_judge_java/epi/IsListPalindromic.java
@@ -1,11 +1,27 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsListPalindromic {
   @EpiTest(testDataFile = "is_list_palindromic.tsv")
 
   public static boolean isLinkedListAPalindrome(ListNode<Integer> L) {
-    // TODO - you fill in here.
+    ListNode<Integer> slow = L, fast = L;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+
+    ListNode<Integer> firstHalfIter = L;
+    ListNode<Integer> secondHalfIter = ReverseList.reverseList(slow);
+    while (firstHalfIter != null && secondHalfIter != null) {
+      if (!firstHalfIter.data.equals(secondHalfIter.data)) {
+        return false;
+      }
+      firstHalfIter = firstHalfIter.next;
+      secondHalfIter = secondHalfIter.next;
+    }
     return true;
   }
 
@@ -13,7 +29,8 @@ public class IsListPalindromic {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "IsListPalindromic.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
