f0a61d31cb5f565d806cce06e2a6696295188941
coupcakes <suhcheollie@gmail.com>
2024-04-12 16:28:39 -0700

    Add 8.6 tree level order java solution

--- epi_judge_java/epi/TreeLevelOrder.java
+++ epi_judge_java/epi/TreeLevelOrder.java
@@ -1,22 +1,41 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+
 public class TreeLevelOrder {
   @EpiTest(testDataFile = "tree_level_order.tsv")
 
-  public static List<List<Integer>>
-  binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+  public static List<List<Integer>> binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
+    List<List<Integer>> res = new ArrayList<>();
+    if (tree == null) {
+      return res;
+    }
+    List<BinaryTreeNode<Integer>> nodesToProcess = new LinkedList<BinaryTreeNode<Integer>>();
+    nodesToProcess.add(tree);
+
+    while (!nodesToProcess.isEmpty()) {
+      res.add(nodesToProcess.stream().map(n -> n.data).toList());
+      nodesToProcess = nodesToProcess.stream()
+          .map(n -> Arrays.asList(n.left, n.right))
+          .flatMap(List::stream)
+          .filter(c -> c != null)
+          .toList();
+    }
+    return res;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "TreeLevelOrder.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
