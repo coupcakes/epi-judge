f9e08da4545e72d473f80dd7dc445b5272721e46
coupcakes <suhcheollie@gmail.com>
2024-04-12 09:34:50 -0700

    Add 8.5 sunset views java solution

--- epi_judge_java/epi/SunsetView.java
+++ epi_judge_java/epi/SunsetView.java
@@ -1,18 +1,40 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+
 public class SunsetView {
-  public static List<Integer>
-  examineBuildingsWithSunset(Iterator<Integer> sequence) {
-    // TODO - you fill in here.
-    return null;
+  private static class BuildingWithHeight {
+    Integer id;
+    Integer height;
+
+    public BuildingWithHeight(Integer id, Integer height) {
+      this.id = id;
+      this.height = height;
+    }
   }
+
+  public static List<Integer> examineBuildingsWithSunset(Iterator<Integer> sequence) {
+    Deque<BuildingWithHeight> candidates = new ArrayDeque<BuildingWithHeight>();
+    int idx = 0;
+    while (sequence.hasNext()) {
+      Integer currHeight = sequence.next();
+      while (!candidates.isEmpty() && Integer.compare(currHeight, candidates.peekFirst().height) >= 0) {
+        candidates.removeFirst();
+      }
+      candidates.addFirst(new BuildingWithHeight(idx++, currHeight));
+    }
+    return candidates.stream().map(c -> c.id).collect(Collectors.toList());
+  }
+
   @EpiTest(testDataFile = "sunset_view.tsv")
-  public static List<Integer>
-  examineBuildingsWithSunsetWrapper(List<Integer> sequence) {
+  public static List<Integer> examineBuildingsWithSunsetWrapper(List<Integer> sequence) {
     return examineBuildingsWithSunset(sequence.iterator());
   }
 
@@ -20,7 +42,8 @@ public class SunsetView {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "SunsetView.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
