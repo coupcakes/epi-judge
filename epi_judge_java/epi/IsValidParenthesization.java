c537235957586e593232b4243dc19858abfac889
coupcakes <suhcheollie@gmail.com>
2024-04-11 18:21:33 -0700

    Add 8.3 is valid parenthesization java solution

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -1,11 +1,28 @@
 package epi;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsValidParenthesization {
   @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
 
   public static boolean isWellFormed(String s) {
-    // TODO - you fill in here.
+    Deque<Character> stack = new ArrayDeque<Character>();
+    final Map<Character, Character> LOOKUP = Map.of(
+        '[', ']',
+        '(', ')',
+        '{', '}');
+    for (int i = 0; i < s.length(); i++) {
+      if (LOOKUP.get(s.charAt(i)) != null) {
+        stack.addFirst(s.charAt(i));
+      } else if (stack.isEmpty() || LOOKUP.get(stack.removeFirst()) != s.charAt(i)) {
+        return false;
+      }
+    }
     return true;
   }
 
@@ -13,7 +30,8 @@ public class IsValidParenthesization {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "IsValidParenthesization.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
