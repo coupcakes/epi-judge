54c95d47c13f9fec1de943e796e9356197e4397a
coupcakes <suhcheollie@gmail.com>
2024-04-25 11:10:27 -0700

    Add 9.7 Tree inorder java solution

--- epi_judge_java/epi/TreeInorder.java
+++ epi_judge_java/epi/TreeInorder.java
@@ -1,8 +1,13 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
+
 public class TreeInorder {
 
   private static class NodeAndState {
@@ -10,7 +15,7 @@ public class TreeInorder {
     public Boolean leftSubtreeTraversed;
 
     public NodeAndState(BinaryTreeNode<Integer> node,
-                        Boolean leftSubtreeTraversed) {
+        Boolean leftSubtreeTraversed) {
       this.node = node;
       this.leftSubtreeTraversed = leftSubtreeTraversed;
     }
@@ -18,15 +23,32 @@ public class TreeInorder {
 
   @EpiTest(testDataFile = "tree_inorder.tsv")
   public static List<Integer> inorderTraversal(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> res = new ArrayList<Integer>();
+    Deque<NodeAndState> stack = new ArrayDeque<NodeAndState>();
+    stack.addFirst(new NodeAndState(tree, false));
+
+    while (!stack.isEmpty()) {
+      NodeAndState nodeAndState = stack.removeFirst();
+      if (nodeAndState.node != null) {
+        if (nodeAndState.leftSubtreeTraversed) {
+          res.add(nodeAndState.node.data);
+        } else {
+          stack.addFirst(new NodeAndState(nodeAndState.node.right, false));
+          stack.addFirst(new NodeAndState(nodeAndState.node, true));
+          stack.addFirst(new NodeAndState(nodeAndState.node.left, false));
+        }
+      }
+    }
+
+    return res;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "TreeInorder.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
