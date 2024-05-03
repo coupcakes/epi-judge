66c87c2648e401e2f0c00337dc7d8444ef1f6f9b
coupcakes <suhcheollie@gmail.com>
2024-05-02 18:33:53 -0700

    Add 9.11 Tree from preorder inorder java solution

--- epi_judge_java/epi/TreeFromPreorderInorder.java
+++ epi_judge_java/epi/TreeFromPreorderInorder.java
@@ -1,22 +1,44 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class TreeFromPreorderInorder {
   @EpiTest(testDataFile = "tree_from_preorder_inorder.tsv")
 
-  public static BinaryTreeNode<Integer>
-  binaryTreeFromPreorderInorder(List<Integer> preorder, List<Integer> inorder) {
-    // TODO - you fill in here.
-    return null;
+  public static BinaryTreeNode<Integer> binaryTreeFromPreorderInorder(List<Integer> preorder, List<Integer> inorder) {
+    return helper(preorder, 0, preorder.size(), 0, inorder.size(),
+        IntStream.range(0, inorder.size()).boxed().collect(Collectors.toMap(x -> inorder.get(x), x -> x)));
+  }
+
+  private static BinaryTreeNode<Integer> helper(List<Integer> preorder, int preorderStart, int preorderEnd,
+      int inorderStart, int inorderEnd, Map<Integer, Integer> inorderIdxMap) {
+    if (preorderStart >= preorderEnd || inorderStart >= inorderEnd) {
+      return null;
+    }
+
+    int rootInorderIdx = inorderIdxMap.get(preorder.get(preorderStart));
+    int leftSubtreeSize = rootInorderIdx - inorderStart;
+    return new BinaryTreeNode<Integer>(
+        preorder.get(preorderStart),
+        helper(preorder, preorderStart + 1, preorderStart + 1 + leftSubtreeSize, inorderStart, rootInorderIdx,
+            inorderIdxMap),
+        helper(preorder, preorderStart + 1 + leftSubtreeSize, preorderEnd, rootInorderIdx + 1, inorderEnd,
+            inorderIdxMap));
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "TreeFromPreorderInorder.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
