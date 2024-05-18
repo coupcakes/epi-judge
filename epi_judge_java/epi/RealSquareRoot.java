45daa97e9f1cd27acd7fde5ded455036c560936a
coupcakes <suhcheollie@gmail.com>
2024-05-17 20:29:55 -0700

    Add 11.5 compute real square root java solution

--- epi_judge_java/epi/RealSquareRoot.java
+++ epi_judge_java/epi/RealSquareRoot.java
@@ -1,19 +1,53 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class RealSquareRoot {
   @EpiTest(testDataFile = "real_square_root.tsv")
 
   public static double squareRoot(double x) {
-    // TODO - you fill in here.
-    return 0.0;
+    double left, right;
+    if (x < 1.0) {
+      left = x;
+      right = 1.0;
+    } else {
+      left = 1.0;
+      right = x;
+    }
+    while (compare(left, right) != Ordering.EQUAL) {
+      double mid = left + ((right - left) / 2);
+      double midSqrd = mid * mid;
+      if (compare(midSqrd, x) == Ordering.LARGER) {
+        right = mid;
+      } else {
+        left = mid;
+      }
+    }
+    return left;
+  }
+
+  private static enum Ordering {
+    LARGER, EQUAL, SMALLER
+  };
+
+  private static Ordering compare(double a, double b) {
+    final double EPSILON = 0.0000001;
+    double diff = (a - b) / Math.max(Math.abs(a), Math.abs(b));
+    if (diff < -EPSILON) {
+      return Ordering.SMALLER;
+    } else if (diff > EPSILON) {
+      return Ordering.LARGER;
+    }
+    return Ordering.EQUAL;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "RealSquareRoot.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
