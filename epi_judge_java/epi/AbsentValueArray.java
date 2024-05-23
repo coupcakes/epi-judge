c79144522c3ce0decd18901f0091cb5cd3fcb340
coupcakes <suhcheollie@gmail.com>
2024-05-23 13:30:48 -0700

    Add 11.9 absent value array java solution

--- epi_judge_java/epi/AbsentValueArray.java
+++ epi_judge_java/epi/AbsentValueArray.java
@@ -1,15 +1,51 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 
+import java.util.BitSet;
 import java.util.List;
+
 public class AbsentValueArray {
 
   public static int findMissingElement(Iterable<Integer> stream) {
-    // TODO - you fill in here.
+    final int NUM_BUCKETS = 1 << 16;
+    int[] counts = new int[NUM_BUCKETS];
+    java.util.Iterator<Integer> s = stream.iterator();
+    while (s.hasNext()) {
+      int prefix = s.next() >>> 16;
+      counts[prefix]++;
+    }
+
+    final int BUCKET_SIZE = 1 << 16;
+    int candidatePrefix = 0;
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+      if (counts[i] < BUCKET_SIZE) {
+        candidatePrefix = counts[i];
+        break;
+      }
+    }
+
+    BitSet candidates = new BitSet(BUCKET_SIZE);
+    s = stream.iterator();
+    while (s.hasNext()) {
+      int x = s.next();
+      int prefix = x >>> 16;
+      if (prefix == candidatePrefix) {
+        int lsb = ((1 << 16) - 1) & x;
+        candidates.set(lsb);
+      }
+    }
+
+    for (int i = 0; i < BUCKET_SIZE; i++) {
+      if (!candidates.get(i)) {
+        return (candidatePrefix << 16) | i;
+      }
+    }
     return 0;
   }
+
   @EpiTest(testDataFile = "absent_value_array.tsv")
   public static void findMissingElementWrapper(List<Integer> stream)
       throws Exception {
@@ -27,7 +63,8 @@ public class AbsentValueArray {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "AbsentValueArray.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
