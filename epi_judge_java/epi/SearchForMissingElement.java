624bc515466eab74d622e37cc4b35cbae8044247
coupcakes <suhcheollie@gmail.com>
2024-05-24 21:58:31 -0700

    Add 11.10 search for missing element java solution

--- epi_judge_java/epi/SearchForMissingElement.java
+++ epi_judge_java/epi/SearchForMissingElement.java
@@ -1,11 +1,13 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 
 import java.util.List;
+
 public class SearchForMissingElement {
-  @EpiUserType(ctorParams = {Integer.class, Integer.class})
+  @EpiUserType(ctorParams = { Integer.class, Integer.class })
 
   public static class DuplicateAndMissing {
     public Integer duplicate;
@@ -25,7 +27,7 @@ public class SearchForMissingElement {
         return false;
       }
 
-      DuplicateAndMissing that = (DuplicateAndMissing)o;
+      DuplicateAndMissing that = (DuplicateAndMissing) o;
 
       if (!duplicate.equals(that.duplicate)) {
         return false;
@@ -49,15 +51,34 @@ public class SearchForMissingElement {
   @EpiTest(testDataFile = "find_missing_and_duplicate.tsv")
 
   public static DuplicateAndMissing findDuplicateMissing(List<Integer> A) {
-    // TODO - you fill in here.
-    return new DuplicateAndMissing(0, 0);
+    int dupXorMissing = 0;
+    for (int i = 0; i < A.size(); i++) {
+      dupXorMissing ^= i ^ A.get(i);
+    }
+    int differBit = dupXorMissing & (~(dupXorMissing - 1));
+    int missingOrDup = 0;
+    for (int i = 0; i < A.size(); i++) {
+      if ((i & differBit) != 0) {
+        missingOrDup ^= i;
+      }
+      if ((A.get(i) & differBit) != 0) {
+        missingOrDup ^= A.get(i);
+      }
+    }
+    for (int i = 0; i < A.size(); i++) {
+      if (A.get(i) == missingOrDup) {
+        return new DuplicateAndMissing(missingOrDup, dupXorMissing ^ missingOrDup);
+      }
+    }
+    return new DuplicateAndMissing(dupXorMissing ^ missingOrDup, missingOrDup);
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "SearchForMissingElement.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
