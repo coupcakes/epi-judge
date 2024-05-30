b9c603cd48fc286485d5f1c476663c633267b46f
coupcakes <suhcheollie@gmail.com>
2024-05-29 19:08:08 -0700

    Add 12.6 smallest subarray covering set more efficient java solution

--- epi_judge_java/epi/SmallestSubarrayCoveringSet.java
+++ epi_judge_java/epi/SmallestSubarrayCoveringSet.java
@@ -6,11 +6,10 @@ import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class SmallestSubarrayCoveringSet {
 
@@ -27,33 +26,68 @@ public class SmallestSubarrayCoveringSet {
 
   public static Subarray findSmallestSubarrayCoveringSet(List<String> paragraph,
       Set<String> keywords) {
-    Map<String, Long> keywordsToCover = keywords.stream()
-        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
+    LinkedHashMap<String, Integer> dict = new LinkedHashMap<String, Integer>(keywords.size(), 1, true);
+    for (String keyword : keywords) {
+      dict.put(keyword, null);
+    }
+    int numKeywordsSeenSoFar = 0;
     Subarray res = new Subarray(-1, -1);
-    int remainingToCover = keywords.size();
-    for (int left = 0, right = 0; right < paragraph.size(); right++) {
-      String currStr = paragraph.get(right);
-      if (keywordsToCover.containsKey(currStr) && keywordsToCover.put(currStr, keywordsToCover.get(currStr) - 1) >= 1) {
-        remainingToCover--;
-      }
-
-      while (remainingToCover == 0) {
-        if ((res.start == -1 && res.end == -1) || (right - left < res.end - res.start)) {
-          res.start = left;
-          res.end = right;
+    for (int i = 0; i < paragraph.size(); i++) {
+      String currString = paragraph.get(i);
+      if (dict.containsKey(currString)) {
+        if (dict.get(currString) == null) {
+          numKeywordsSeenSoFar++;
         }
-
-        String currLeftStr = paragraph.get(left);
-        if (keywordsToCover.containsKey(currLeftStr)
-            && keywordsToCover.put(currLeftStr, keywordsToCover.get(currLeftStr) + 1) >= 0) {
-          remainingToCover++;
+        dict.put(currString, i);
+        if (numKeywordsSeenSoFar == keywords.size()) {
+          if ((res.start == -1 && res.end == -1) || (i - getFirstOccurrence(dict) < res.end - res.start)) {
+            res.start = getFirstOccurrence(dict);
+            res.end = i;
+          }
         }
-        left++;
       }
     }
 
     return res;
+    // Map<String, Long> keywordsToCover = keywords.stream()
+    // .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+    // Subarray res = new Subarray(-1, -1);
+    // int remainingToCover = keywords.size();
+    // for (int left = 0, right = 0; right < paragraph.size(); right++) {
+    // String currStr = paragraph.get(right);
+    // if (keywordsToCover.containsKey(currStr) && keywordsToCover.put(currStr,
+    // keywordsToCover.get(currStr) - 1) == 1) {
+    // remainingToCover--;
+    // }
+
+    // while (remainingToCover == 0) {
+    // if ((res.start == -1 && res.end == -1) || (right - left < res.end -
+    // res.start)) {
+    // res.start = left;
+    // res.end = right;
+    // }
+
+    // String currLeftStr = paragraph.get(left);
+    // if (keywordsToCover.containsKey(currLeftStr)
+    // && keywordsToCover.put(currLeftStr, keywordsToCover.get(currLeftStr) + 1) ==
+    // 0) {
+    // remainingToCover++;
+    // }
+    // left++;
+    // }
+    // }
+
+    // return res;
+  }
+
+  private static Integer getFirstOccurrence(LinkedHashMap<String, Integer> dict) {
+    Integer idx = null;
+    for (Map.Entry<String, Integer> entry : dict.entrySet()) {
+      idx = entry.getValue();
+      break;
+    }
+    return idx;
   }
 
   @EpiTest(testDataFile = "smallest_subarray_covering_set.tsv")
