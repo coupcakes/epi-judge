50ac258327b6407aba8a2c753b8b0f28340674d3
coupcakes <suhcheollie@gmail.com>
2024-05-31 19:07:46 -0700

    Add 12.9 Longest contained interval java solution

--- epi_judge_java/epi/LongestContainedInterval.java
+++ epi_judge_java/epi/LongestContainedInterval.java
@@ -1,14 +1,34 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 public class LongestContainedInterval {
   @EpiTest(testDataFile = "longest_contained_interval.tsv")
 
   public static int longestContainedRange(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
+    Set<Integer> toProcess = new HashSet<>(A);
+    int longestContainedRange = 0;
+    while (!toProcess.isEmpty()) {
+      Integer val = toProcess.iterator().next();
+      toProcess.remove(val);
+
+      int lowerBound = val - 1;
+      while (toProcess.contains(lowerBound)) {
+        toProcess.remove(lowerBound);
+        lowerBound--;
+      }
+
+      int upperBound = val + 1;
+      while (toProcess.contains(upperBound)) {
+        toProcess.remove(upperBound);
+        upperBound++;
+      }
+
+      longestContainedRange = Math.max(longestContainedRange, upperBound - lowerBound - 1);
+    }
+    return longestContainedRange;
   }
 
   public static void main(String[] args) {
