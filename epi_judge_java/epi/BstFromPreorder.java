14ee625ccc660aa54ee9a97c3208fc7d688b167b
coupcakes <suhcheollie@gmail.com>
2024-06-06 09:55:39 -0700

    Add 14.5 Reconstruct BST from traversal data java solution

--- epi_judge_java/epi/BstFromPreorder.java
+++ epi_judge_java/epi/BstFromPreorder.java
@@ -1,15 +1,33 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-
 import java.util.List;
 public class BstFromPreorder {
+  private static int rootIdx;
+  
   @EpiTest(testDataFile = "bst_from_preorder.tsv")
+
 
   public static BstNode<Integer>
   rebuildBSTFromPreorder(List<Integer> preorderSequence) {
-    // TODO - you fill in here.
-    return null;
+    rootIdx = 0;
+    return bstHelper(preorderSequence, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+
+  private static BstNode<Integer> bstHelper(List<Integer> preorderSeq, int lower, int upper) {
+    if (rootIdx == preorderSeq.size()) {
+      return null;
+    }
+
+    int rootVal = preorderSeq.get(rootIdx);
+    if (rootVal < lower || rootVal > upper) {
+      return null;
+    }
+
+    rootIdx++;
+    BstNode<Integer> leftSubtree = bstHelper(preorderSeq, lower, rootVal);
+    BstNode<Integer> rightSubtree = bstHelper(preorderSeq, rootVal, upper);
+    return new BstNode<>(rootVal, leftSubtree, rightSubtree);
   }
 
   public static void main(String[] args) {
